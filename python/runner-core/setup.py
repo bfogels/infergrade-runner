ec991ef388df1797c68cd7fc2f06ba9871bedf34
Brian Fogelson <68251468+bfogels@users.noreply.github.com>
2026-05-13 08:42:38 -0400

    Release Runner v0.2.21 preview (#214)
    Co-authored-by: Brian Fogelson <brianf888@gmail.com>

--- python/runner-core/setup.py
+++ python/runner-core/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="infergrade-runner-core",
-    version="0.2.20",
+    version="0.2.21",
     description="Portable benchmarking runner core for InferGrade.",
     author="Brian Fogelson",
     package_dir={"": "src"},
